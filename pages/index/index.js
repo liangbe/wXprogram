a9cec46dcbdd9cb3d892ff9fba43be1914abe120
liangbe <9169lucifer@163.com>
2018-08-27 12:00:49 +0800

    33

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: '设置Git Extension 成功密码！！！',
+    motto: '设置Git Extension 成功！！！',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
